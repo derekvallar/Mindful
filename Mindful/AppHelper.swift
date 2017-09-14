9ee4063869f717e49d27a9b9f8d27abf6b3ce6c4
derekvallar <derekvallar@gmail.com>
2017-09-13 18:10:13 -0700

    Initialized Mindful with main view controller.

---
+++ Mindful/AppHelper.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Mindful
+//
+//  Created by Derek Vitaliano Vallar on 9/12/17.
+//  Copyright © 2017 Derek Vallar. All rights reserved.
+//
+
+import UIKit
+
+enum Constants {
+    static let AppName = "Mindful"
+}
+
+extension UIView {
+
+}
